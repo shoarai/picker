2c1e8d74595bffe26d4c71ae72e72d8e9191b294
shoarai <shohei.arai.52@gmail.com>
2014-10-19 16:25:29 +0900

    1番目のタスクが取り消し線にならないバグ修正

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,9 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '192.168.0.4',
+        hostname: '192.168.0.3',
+//        hostname: '127.0.0.1',
+//        hostname: '0.0.0.0',
 //        hostname: 'localhost',
         livereload: 35729
       },
