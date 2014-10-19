e9c6e529ea97d43af65d9a817779d0ca2ca9ee85
shoarai <shohei.arai.52@gmail.com>
2014-10-19 17:39:06 +0900

    タスクの順番入替

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -10,7 +10,7 @@
 angular.module('pickerApp')
   .controller('TaskCtrl', function ($scope, storage) {
    
-    storage.bind($scope, 'tasks', {defaultValue : []});
+    storage.bind($scope, 'tasks', {defaultValue : [], storeName: 'tasks'});
     
     // $scope.tasks = [
       // {'body':'do this 1', 'done':false},
@@ -19,8 +19,39 @@ angular.module('pickerApp')
       // {'body':'do this 4', 'done':false},
     // ];
     
+    function getSorted(arr, sortArr) {
+      var result = [],
+          i = 0;
+      for(; i < arr.length; i++) {
+        result[i] = arr[sortArr[i]];
+      }
+      return result;
+    }
+    
+    var mySort = document.getElementById("tasks");
+    new Sortable(mySort, {
+      onUpdate: function(evt) {
+        // get new sort order based on indexes
+        var newSortIndexes = [],
+            liElements = mySort.getElementsByTagName("li"),
+            i = 0;
+        for (; i < liElements.length; i++) {
+          newSortIndexes.push(liElements[i].getAttribute('data-index'));
+        }
+        
+        // process change
+        $scope.newSortIndexes = newSortIndexes;
+        $scope.tasks = getSorted($scope.tasks, newSortIndexes);
+        $scope.$apply();
+      }
+    });
+    
+    
     $scope.addNew = function() {
-      $scope.tasks.push({'body':$scope.newTaskBody, 'done':false});
+      $scope.tasks.push({
+        'body': $scope.newTaskBody,
+        'done': false
+      });
       $scope.newTaskBody = '';
     };
     $scope.deleteDone = function() {
